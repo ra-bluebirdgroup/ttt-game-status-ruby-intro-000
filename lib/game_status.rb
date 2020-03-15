c1c9cb4fdcffa375b3c7db1effb93dd9c46f96c7
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-15 00:56:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,33 +16,85 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if board.empty?
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
     return false
   end
-  
-WIN_COMBINATIONS.each do |combo|
-  win_pos = combo[i]
-  local_win_combination_X = []
-  local_win_combination_O = []
+end
 
-board.each_with_index do |pos|idx|
-if pos == "X" && idx == win_pos[i]
-  local_win_combination_X << win_pos[i]
-
-elsif pos == "O" && idx = win_pos[i]
-  local_win_combination_O  << win_pos[i]
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
   end
- end
-
-if combo == local_win_combination_X
-  puts "X won"
-  return local_win_combination_X
-elsif combo == local_win_combination_y
-  puts "O won"
-  return local_win_combination_y
-else
-  puts "tie"
 end
 
- end
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
 end
+# def won?(board)
+#   if board.empty?
+#     return false
+#   end
+#   
+# WIN_COMBINATIONS.each do |combo|
+#   win_pos = combo[i]
+#   local_win_combination_X = []
+#   local_win_combination_O = []
+# 
+# board.each_with_index do |pos|idx|
+# if pos == "X" && idx == win_pos[i]
+#   local_win_combination_X << win_pos[i]
+# 
+# elsif pos == "O" && idx = win_pos[i]
+#   local_win_combination_O  << win_pos[i]
+#   end
+#  end
+# 
+# if combo == local_win_combination_X
+#   puts "X won"
+#   return local_win_combination_X
+# elsif combo == local_win_combination_y
+#   puts "O won"
+#   return local_win_combination_y
+# else
+#   puts "tie"
+# end
+# 
+#  end
+# end
